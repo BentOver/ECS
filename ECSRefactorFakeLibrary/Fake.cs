7987c102669d81d21ec8e8027dd05aa7590397f2
ApolloISE <mads.apollo@gmail.com>
2022-02-17 13:13:04 +0100

    Added fake library

---
+++ ECSRefactorFakeLibrary/Fake.cs
@@ -0,0 +1,53 @@
+﻿using ECSRefactor;
+
+namespace ECSRefactorFakeLibrary
+{
+    public class FakeHeater : IHeater
+    {
+        public void TurnOn()
+        {
+            System.Console.WriteLine("Heater is on");
+            ++Count;
+        }
+
+        public void TurnOff()
+        {
+            System.Console.WriteLine("Heater is off");
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+
+        public void ResetCount()
+        {
+            Count = 0;
+        }
+
+        public int Count
+        {
+            get;
+            set;
+        }
+    }
+    public class FakeTempSensor : ITempSensor
+    {
+        private int temp_;
+        public int GetTemp()
+        {
+            return temp_;
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+
+        public void SetTemp(int temp)
+        {
+            temp_ = temp;
+        }
+
+    }
+}
